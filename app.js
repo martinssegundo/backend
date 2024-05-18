c73302abf90418473b0bdf6454d6d6b7d1149c6d
Isaque Böck <isaquebockdos@gmail.com>
2024-05-18 00:08:50 -0300

    feat: init project

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import {createClient} from '@supabase/supabase-js'
+import morgan from 'morgan'
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
+
+// using morgan for logs
+app.use(morgan('combined'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const supabase = createClient('https://ujmvnzcbcgavpknmqcfx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbXZuemNiY2dhdnBrbm1xY2Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3MjkyNTEsImV4cCI6MjAzMTMwNTI1MX0.B0AxNh-3Z2ZrZc5F2xnHetG2deK1MXWaHbtbsRr9vPo');
+
+app.get('/challenges', async (req, res) => {
+  const {data, error} = await supabase
+      .from('challenges')
+      .select()
+  res.send(data);
+});
+
+app.get('/auth', async (req, res) => {
+  const data = await supabase.auth.signInWithOAuth({
+    provider: 'github'
+  })
+
+  res.send(data);
+})
+
+app.listen(3000, () => {
+  console.log(`> Ready on http://localhost:3000`);
+});
+
