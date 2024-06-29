92378a3c36ef43cefa60c66941836b8635eb855f
Isaque Böck <isaquebockdos@gmail.com>
2024-06-29 00:09:51 -0300

    feat: enable all cors origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://boostingdevs.com', 'http://localhost:4200'],
+    origin: '*',
   })
   await app.listen(3000);
 }
